fb0fd4a550aea6e0806847e0f4853843fd85f2b8
qinfengling <Fengling.Qin@gmail.com>
2013-01-26 18:10:17 +0800

    增加helloworld样例

---
+++ helloworld/src/helloworld.c
@@ -0,0 +1,12 @@
+/****************
+ * * Helloworld.c
+ * * The most simplistic C program ever written.
+ * * An epileptic monkey on crack could write this code.
+ * *****************/
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+	printf("Hell! O' world, Mikeqin test!\n\n");
+	return 0;
+}
